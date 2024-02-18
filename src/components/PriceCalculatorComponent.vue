8bb96eb6475f618cc2248a31d808b04f0c32985d
Luciano Nahuel Vitale <lvitale@ausa.com.ar>
2024-02-18 02:22:06 -0300

    Implemented sorted techniques using local storage

--- src/components/PriceCalculatorComponent.vue
+++ src/components/PriceCalculatorComponent.vue
@@ -3,9 +3,13 @@ import axios from 'axios'
 import { type Ref, onMounted, ref, computed, reactive } from 'vue'
 import InputNumber from 'primevue/inputnumber'
 import Button from 'primevue/button'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const pesos = ref({})
 const dolares = ref({})
+
+const priority = ref(-1)
 
 const swapped = ref(false)
 
@@ -14,8 +18,8 @@ type Price = {
   compra: number
 }
 
-const getPrices = (obj: { [x: string]: any; }, k: string = ""): Price[] => {
-    let results = {} as Price[];
+const getPrices = (obj: { [x: string]: any }, k: string = ""): { [x: string]: Price } => {
+    let results = {} as { [x: string]: Price };
 
     // Iterate over each key-value pair in the object
     for (let key in obj) {
@@ -23,14 +27,20 @@ const getPrices = (obj: { [x: string]: any; }, k: string = ""): Price[] => {
             // If the value is an object, recursively call the function
             results = {...results, ...getPrices(obj[key], `${k}-${key}`)};
         } else if (key === 'price') {
+            const priorityStoraged = localStorage.getItem(k)
+            priority.value = priorityStoraged ? parseInt(priorityStoraged) : priority.value + 1
             results[k] = {
                 "venta": obj[key],
-                "compra": obj[key]
+                "compra": obj[key],
+                "priority": priority.value
             } as Price;
         } else if (key === 'ask') {
+            const priorityStoraged = localStorage.getItem(k)
+            priority.value = priorityStoraged ? parseInt(priorityStoraged) : priority.value + 1
             results[k] = {
                 "venta": obj[key],
-                "compra": obj['bid']
+                "compra": obj['bid'],
+                "priority": priority.value
             } as Price;
         }
     }
@@ -78,22 +88,44 @@ const pesosComputed = computed(() => {
     }
     return result
 })
+
+const onRowReorder = (event) => {
+    console.log(event)
+    console.log(event.value);
+    event.value.forEach((element, index) => {
+        prices.value[element.cambio].priority = index
+        localStorage.setItem(element.cambio, index.toString())
+    });
+};
 </script>
 
 <template>
-    <div v-for="price in Object.keys(prices)">
-        <a>
-            {{ price.split('-').map(
+    <DataTable :value="Object.entries(prices).map(([currency, values]) => ({ cambio: currency, ...values }))"
+        :reorderableColumns="true" sort-field="priority" :sort-order="1" @column-reorder="" @row-reorder="onRowReorder">
+        <Column rowReorder headerStyle="width: 3rem" :reorderableColumn="false" />
+        <Column field="cambio" header="Tipo de cambio">
+            <template #body="slotProps">
+                {{ slotProps.data.cambio.split('-').map(
                 (word, index) => index <= 1 ? word.charAt(0).toUpperCase() + word.slice(1) : word.toUpperCase()
-            ).join(' ') }} {{ `${(!swapped ? prices[price].venta: prices[price].compra).toFixed(2)} ARS` }}
-        </a>
-
-        <InputNumber :id="`pesos${price}`" v-if="!swapped" mode="currency" currency="ARS" v-model="pesos[price]" />
-        <InputNumber :id="`dolar${price}`" v-else mode="currency" currency="USD" v-model="dolares[price]" />
-
-        <Button type="button" @click="swapCoins"><i class="pi pi-sync"></i></Button>
-        
-        <InputNumber :id="`dolar${price}`" v-if="!swapped" mode="currency" currency="USD" v-model="dolaresComputed[price]" :disabled="true" />
-        <InputNumber :id="`pesos${price}`" v-else mode="currency" currency="ARS" v-model="pesosComputed[price]" :disabled="true" />
-    </div>
+            ).join(' ') }} {{ `${(!swapped ? slotProps.data.venta: slotProps.data.compra).toFixed(2)} ARS` }}
+            </template>
+        </Column>
+        <Column field="monto-a-convertir" header="Monto a convertir">
+            <template #body="slotProps">
+                <InputNumber :id="`pesos${slotProps.data.cambio}`" v-if="!swapped" mode="currency" currency="ARS" v-model="pesos[slotProps.data.cambio]" />
+                <InputNumber :id="`dolar${slotProps.data.cambio}`" v-else mode="currency" currency="USD" v-model="dolares[slotProps.data.cambio]" />
+            </template>
+        </Column>
+        <Column field="swap" header="Swap">
+            <template #body>
+                <Button type="button" @click="swapCoins"><i class="pi pi-sync"></i></Button>
+            </template>
+        </Column>
+        <Column field="monto-convertido" header="Monto convertido" >
+            <template #body="slotProps">
+                <InputNumber :id="`dolar${slotProps.data.cambio}`" v-if="!swapped" mode="currency" currency="USD" v-model="dolaresComputed[slotProps.data.cambio]" :disabled="true" />
+                <InputNumber :id="`pesos${slotProps.data.cambio}`" v-else mode="currency" currency="ARS" v-model="pesosComputed[slotProps.data.cambio]" :disabled="true" />
+            </template>
+        </Column>
+    </DataTable>
 </template>
